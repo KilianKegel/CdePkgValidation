d7bab2734e09c3a502b76063b9ea1eabc8d02efb
KG <kilian_kegel@hotmail.com>
2020-02-22 06:00:49 +0100

    add PreMemInit DEPEX to clock/main.c

--- TIME_H/clock/main.c
+++ TIME_H/clock/main.c
@@ -49,16 +49,21 @@
 
 @subsection Parm_sec Command line parameters
 */
+#pragma warning(disable:4005)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <CDE.h>
 
+#include <Uefi.h>
+#include <PiPei.h>
 #define COUNT 20
 #define FRQ   1
 
 extern char _CdeGetCurrentPrivilegeLevel(void); // check emulation mode
+
+extern char* strefierror(EFI_STATUS Status);
 
 //#include <uefi.h>
 
@@ -75,6 +80,19 @@ int main(int argc, char** argv) {
     CDEMOFINE((MFNINF(1) "##################################################################\n"));
     CDEMOFINE((MFNINF(1) "########################## Test clock() in %s %s\n", CDE_CONFIGURATION_STRING, CDE_PLATFORM_STRING));
     CDEMOFINE((MFNINF(1) "##################################################################\n"));
+    if (1)
+    {
+        EFI_STATUS Status = 0LL;
+        EFI_PEI_SERVICES** PeiServices = (void*)(argv[-1]);    //PeiServices is passed in argv[-1]
+        //EFI_PEI_FILE_HANDLE FileHandle = (void*)(argv[-2]);    //FileHandle  is passed in argv[-2]
+
+        static EFI_GUID gCdePeiTmpTest0Guid = CDE_PEI_TMPTEST0_GUID;
+        static EFI_PEI_PPI_DESCRIPTOR CdeLoadOptionsPpiList = {/*Flags*/(EFI_PEI_PPI_DESCRIPTOR_PPI | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),/**Guid*/&gCdePeiTmpTest0Guid, /*Ppi*/0 };
+
+        Status = (*PeiServices)->InstallPpi (PeiServices, &CdeLoadOptionsPpiList);
+
+        CDEMOFINE((MFNBAR(1) "====================> %s %llX\n", strefierror(Status), Status));
+    }
 
     for (i = 0; i < argc; i++) {
         //
